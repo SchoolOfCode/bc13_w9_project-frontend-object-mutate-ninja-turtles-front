94d85456c0ae856f5091e9fc30a5c11812314a21
Andrew <andrew@eseeds.com>
2022-11-24 14:22:09 +0000

    correctly pulls data

--- reflections-project/src/components/Summary/Scoreboard/Scoreboard.js
+++ reflections-project/src/components/Summary/Scoreboard/Scoreboard.js
@@ -4,15 +4,22 @@ function Scoreboard() {
   const [data, setData] = useState('');
   const [hello, setHello] = useState('');
   useEffect(() => {
-    setHello(data);
-    console.log(`hello ${hello}`);
+    let pText = JSON.stringify(data[0].subject_title);
+    setHello(pText);
+    console.log(`hello ${hello}, Data ${data}`);
   }, [data]);
 
   async function displayReviews() {
-    const responseJSON = await fetch('http://localhost:3001/api/bootcampers');
+    const responseJSON = await fetch('http://localhost:3001/api/bootcampers', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    console.log(responseJSON);
     let response = await responseJSON.json();
-    console.log(`Data ${JSON.stringify(response.payload[0])}`);
-    setData(JSON.stringify(response.payload));
+    console.log(`Data ${response.payload}`);
+    setData(response.payload);
     //return dataStr;
   }
 
