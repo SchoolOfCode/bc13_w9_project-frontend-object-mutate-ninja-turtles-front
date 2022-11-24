b6ffedd9dae8a6d161ae7fae44ae3d556094c624
Andrew <andrew@eseeds.com>
2022-11-24 16:29:02 +0000

    stops crash on load

--- reflections-project/src/components/Summary/Scoreboard/Scoreboard.js
+++ reflections-project/src/components/Summary/Scoreboard/Scoreboard.js
@@ -1,41 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 function Scoreboard() {
   const [data, setData] = useState('');
   const [hello, setHello] = useState('');
-  useEffect(() => {
-    let pText = JSON.stringify(data[0].subject_title);
-    setHello(pText);
-    console.log(`hello ${hello}, Data ${data}`);
-  }, [data]);
 
-  async function displayReviews() {
+  const fetchData = async () => {
     const responseJSON = await fetch('http://localhost:3001/api/bootcampers', {
       method: 'GET',
       headers: {
         'Content-type': 'application/json',
       },
     });
-    console.log(responseJSON);
     let response = await responseJSON.json();
-    console.log(`Data ${response.payload}`);
+    //console.log(`Data2 ${JSON.stringify(response)}`);
+    console.log('Data', response);
     setData(response.payload);
-    //return dataStr;
-  }
+  };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // async function displayReviews() {
+  //   const responseJSON = await fetch('http://localhost:3001/api/bootcampers', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-type': 'application/json',
+  //     },
+  //   });
+  //   console.log(responseJSON);
+  //   let response = await responseJSON.json();
+  //   console.log(`Data ${response.payload}`);
+  //   setData(response.payload);
+  //   //return dataStr;
+  // }
+  // let pText = data[0].subject_title;
+  // setHello(pText);
+  //console.log(`hello ${hello}, Data ${data}`);
 
   return (
     <div>
-      Scoreboard
-      <button
+      {/* Scoreboard */}
+      <button onClick={fetchData}>Click Me</button>
+      <p>hi{data.length > 0 && data[0].subject_title}</p>
+    </div>
+  );
+}
+
+export default Scoreboard;
+
+/*
+<button
         onClick={() => {
           displayReviews();
         }}
       >
         Display
       </button>
-      <p>hi{hello}</p>
-    </div>
-  );
-}
-
-export default Scoreboard;
+*/
